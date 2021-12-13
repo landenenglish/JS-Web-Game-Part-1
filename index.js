b00f9df6eaa08da693111ec687eec98e3fd2ed8c
landenenglish <englishlanden@gmail.com>
2021-12-13 18:22:23 -0500

    finished + bonus

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-
-function newImage(src, left, bottom) {
-    let newImage = document.createElement('img')
-    newImage.src = src
-    newImage.style.position = 'fixed'
-    newImage.style.left = left
-    newImage.style.bottom = bottom
-    document.body.append(newImage)
+function tile(url, left, bottom, width, height){
+  for(let h = 0; h < height; h++){
+      for(let w = 0; w < width; w++){
+          newImage(url, left + w*100, bottom+h*100)
+      }
+  }
 }
-newImage('assets/green-character.gif', '100px', '100px')
-newImage('assets/pine-tree.png', '450px', '200px')
-newImage('assets/tree.png', '200px', '300px')
-newImage('assets/pillar.png', '350px', '100px')
-newImage('assets/crate.png', '150px', '200px')
-newImage('assets/well.png', '500px', '425px')
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
 
 
-function newItem(src, left, bottom) {
-    newImage(src, left, bottom)
-    let item = document.querySelector('img:last-child')
-    item.addEventListener('click', function () {
-        item.remove()
-    })
+
+
+function newImage(url, left, bottom){
+  let object = document.createElement('img')
+  object.src = url
+  object.style.position = 'fixed'
+  object.style.left = left + 'px'
+  object.style.bottom = bottom + 'px'
+  document.body.append(object)
+  return object
 }
-newItem('assets/sword.png', '500px', '405px')
-newItem('assets/sheild.png', '165px', '185px')
-newItem('assets/staff.png', '600px', '100px')
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+function newItem(url, left, bottom){
+  let object = newImage(url, left, bottom)
+
+  object.addEventListener('dblclick', () => {
+      object.remove()
+  })
+}
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
+
+
+
+
+
+
